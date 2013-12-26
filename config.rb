81e10ce001535bd0c6e7cd629ffab2c3c29f2ef6
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2013-12-26 12:26:15 +0100

    Added initial version.

---
+++ config.rb
@@ -0,0 +1,24 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/app/stylesheets"
+sass_dir = "assets/app/stylesheets"
+images_dir = "assets/app/images"
+javascripts_dir = "assets/app/javascripts"
+
+output_style = :nested
+environment = :development
+
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass assets/app/stylesheets scss && rm -rf sass && mv scss sass
