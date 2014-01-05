7992e71a9e121d95f1feeee973e1f283ab05c34f
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2014-01-05 15:31:57 +0100

    Tiny changes to the app.'s file.

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-var path     = require ("path"),
-	_        = require ("underscore"),
-	express  = require ("express"),
-	request  = require('request'),
-	http     = require('http');
+var path    = require ("path"),
+	_         = require ("underscore"),
+	express   = require ("express"),
+	request   = require('request'),
+	http      = require('http');
 
 var app = express()
 			.use (express.static (__dirname,
-									path.join (__dirname, "bower_components"),
-									path.join (__dirname, "js")))
+				path.join (__dirname, "bower_components"),
+				path.join (__dirname, "js")))
 			.use(express.bodyParser());
 
 var object_json;
+var dataPath = 'http://marijerooze.nl/thesis/graphics/API/';
 
-//var dataPath = 'http://marijerooze.nl/thesis/graphics/API/';
-
-request('http://marijerooze.nl/thesis/graphics/API/', function (error, response, body) {
+request(dataPath, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     object_json = body;
   }
@@ -23,7 +22,6 @@ request('http://marijerooze.nl/thesis/graphics/API/', function (error, response,
 app.get ("/graphics", function( req, res ) {
 	res.send( object_json );
 });
-
 
 var port = process.env.PORT || 2020;  // process.env.PORT is a Heroku setting
 app.listen( port );
