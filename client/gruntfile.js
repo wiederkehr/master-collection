df289a94c5353a2fbd8f148723e2cad42bb74f08
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2014-01-14 14:35:21 +0100

    Rewrote some of the grunt file to meet the current guidelines.

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -3,44 +3,29 @@ module.exports = function(grunt) {
   // makes  grunt.loadNpmTasks('...') not needed
   require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-
-      dirs: {
-        src: 'public/js'
-      },
-
-      jshint: {
-        files: ['Gruntfile.js', 'dev/js/**'],
-        options: {
-          // options here to override JSHint defaults
-          globals: {
-            jQuery: true,
-            console: true,
-            module: true,
-            document: true
-          }
-        }
-      },
-
-      copy: {
-        main: {
-          files: [
-            // includes files within path
-            { expand: true, flatten: true, src: ['bower_components/backbone/backbone-min.js'], dest: 'public/js/lib'},
-            { expand: true, flatten: true, src: ['bower_components/backbone/backbone-min.map'], dest: 'public/js/lib'},         
-            { expand: true, flatten: true, src: ['bower_components/backbone-query-parameters/backbone.queryparams.js'], dest: 'public/js/lib'},        
-            { expand: true, flatten: true, src: ['bower_components/handlebars/handlebars.min.js'], dest: 'public/js/lib'},
-            { expand: true, flatten: true, src: ['bower_components/jquery/jquery.min.js'], dest: 'public/js/lib'},
-            { expand: true, flatten: true, src: ['bower_components/jquery/jquery.min.map'], dest: 'public/js/lib'},
-            { expand: true, flatten: true, src: ['bower_components/underscore/underscore-min.js'], dest: 'public/js/lib'},
-            { expand: true, flatten: true, src: ['bower_components/underscore/underscore-min.map'], dest: 'public/js/lib'},
-            { expand: true, flatten: true, src: ['bower_components/normalize-css/normalize.css'], dest: 'public/css'},
-            { expand: true, flatten: true, src:['bower_components/jslider/bin/jquery.slider.min.js'], dest: 'public/js/lib'},
-            { expand: true, flatten: true, src:['bower_components/jslider/bin/jquery.slider.min.css'], dest: 'public/css'},
-            ]
-          }
-        },
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    dirs: {
+      src: 'public/js'
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path
+          { expand: true, flatten: true, src: ['bower_components/backbone/backbone-min.js'], dest: 'public/js/lib'},
+          { expand: true, flatten: true, src: ['bower_components/backbone/backbone-min.map'], dest: 'public/js/lib'},         
+          { expand: true, flatten: true, src: ['bower_components/backbone-query-parameters/backbone.queryparams.js'], dest: 'public/js/lib'},
+          { expand: true, flatten: true, src: ['bower_components/handlebars/handlebars.min.js'], dest: 'public/js/lib'},
+          { expand: true, flatten: true, src: ['bower_components/jquery/jquery.min.js'], dest: 'public/js/lib'},
+          { expand: true, flatten: true, src: ['bower_components/jquery/jquery.min.map'], dest: 'public/js/lib'},
+          { expand: true, flatten: true, src: ['bower_components/underscore/underscore-min.js'], dest: 'public/js/lib'},
+          { expand: true, flatten: true, src: ['bower_components/underscore/underscore-min.map'], dest: 'public/js/lib'},
+          { expand: true, flatten: true, src: ['bower_components/normalize-css/normalize.css'], dest: 'public/css'},
+          { expand: true, flatten: true, src:['bower_components/jslider/bin/jquery.slider.min.js'], dest: 'public/js/lib'},
+          { expand: true, flatten: true, src:['bower_components/jslider/bin/jquery.slider.min.css'], dest: 'public/css'},
+        ]
+      }
+    },
 
     // process html to set to minified versions
     processhtml: {
@@ -51,15 +36,13 @@ module.exports = function(grunt) {
       }
     },
 
-    // combine all js files
+    // concat and minify js
     concat: {
-        basic_and_extras: {
-          files: {
-            'public/js/app.js': ['dev/js/*.js', 'dev/js/models/*.js', 'dev/js/collections/*.js', 'dev/js/views/*.js', 'dev/js/routers/*.js'],
-           // 'public/css/main.css': ['dev/css/*.css'],
-          },
-        },
-      },
+      dist: {
+        src: ['dev/js/*.js', 'dev/js/models/*.js', 'dev/js/collections/*.js', 'dev/js/views/*.js', 'dev/js/routers/*.js'],
+        dest: 'public/js/app.js'
+      }
+    },
 
     // concat and minify css
     cssmin: {
@@ -70,19 +53,44 @@ module.exports = function(grunt) {
       }
     },
 
-    // minify the js
+    // uglify the js
     uglify: {
-      js: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
         files: {
-          'public/js/app.min.js': ['public/js/app.js'] //save over the newly created script
+          'public/js/app.min.js': ['<%= concat.dist.dest %>']
         }
       }
+    },
+
+    jshint: {
+      files: ['gruntfile.js', 'dev/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true
+        }
+      }
+    },
+
+    watch: {
+      files: ['<%= concat.dist.src %>', 'dev/css/*.css', 'public/data/*.json'],
+      tasks: ['concat', 'uglify', 'cssmin', 'processhtml']
     }
 
   });
 
-  grunt.registerTask("jshint2", ["jshint"]);
-  grunt.registerTask("copy2", ["copy"]);
-  grunt.registerTask("dev", ["concat", "uglify","cssmin", "processhtml"]);
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-processhtml');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'processhtml']);
 
 };
