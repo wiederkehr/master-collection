0616fbc886cea7a24aa07af0639dd2da72a1e6ef
Benjamin Wiederkehr <me@benjaminwiederkehr.com>
2014-01-14 14:31:41 +0100

    Removed unnecessary date formatting.

--- client/dev/js/handlehelpers.js
+++ client/dev/js/handlehelpers.js
@@ -1,20 +1,3 @@
-
-// converts date=20120802 to August, 2013
-var m_names = new Array("January", "February", "March", "April", "May", 
-	"June", "July", "August", "September", "October", "November", "December");
-
-Handlebars.registerHelper("formatDate", function( date ) {
- 	date = String( date );
- 	var year = date.substring( 0, 4 );
-
- 	var month = date.substring(4, 6);
- 	month = month.replace(/^0+/, '');  // regex removes leading zero's
- 	var f_month = m_names[month - 1];	// set month zero based
-
-  	return f_month + ", " + year;
-});
-
-
 /* Image path options
 	http://marijerooze.nl/thesis/graphics/images/guardian/guardian_australiaElections2013.jpg
 	http://marijerooze.nl/thesis/graphics/images/nytimes/nytimes_economytwistandturns.jpg
